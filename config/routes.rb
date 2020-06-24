d82eb557cd8ba15ed9bc79f335e8d086df804bf0
liquidsword <audenrobertson@gmail.com>
2020-06-24 15:10:26 -0400

    created sessions views
    Created sessions home and new. Also added bcrypt for login capablilities.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get ‘/auth/failure’ => redirect(‘/’)
+  #get ‘/auth/failure’ => redirect(‘/’)
 
   root 'sessions#home'
 
