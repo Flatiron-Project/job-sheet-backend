f1d0e6b2cac4d0a0df5978a6078b78d839808d17
liquidsword <audenrobertson@gmail.com>
2020-06-23 22:53:24 -0400

    Update sessions_controller.rb
    fleshed out sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,40 @@
 class SessionsController < ApplicationController
-end
+    def home
+
+    end
+
+    def destroy
+      session.clear
+      redirect_to '/'
+    end
+
+    def google
+      @user = User.find_or_create_by(email: auth["info"]["email"]) do |user|
+        user.username = auth["info"]["first_name"]
+        user.password= SecureRandom.hex(10)
+      end
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        redirect_to '/'
+
+      end
+    end
+
+    def create
+      @user = User.find_by(username: params[:user][:username])
+      if user && user.authenticate(params[:user][:password])
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        flash[:message] = "Incorrect login, please try again!"
+        redirect_to "/login"
+      end
+    end
+
+  private
+    def auth
+        request.env['omniauth.auth']
+    end
+ end
