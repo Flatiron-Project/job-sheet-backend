785eec7d1ce257cce67cac956deb6d873164b76a
Andrew Davisson <akdavisson4@gmail.com>
2020-07-02 09:35:47 -0600

    render json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by_id(params[:id])
-    redirect_to '/' if !@user
+    user = User.find_by_id(params[:id])
+    # redirect_to '/' if !@user
+    render json: user, only: [:id, :username, :email]
   end
 
   private
